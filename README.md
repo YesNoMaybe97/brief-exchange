ca214f1ed0eac00c202273a4cd85b31415310212
Adam Jakś <adamjaks97@gmail.com>
2017-08-24 13:19:45 +0200

    version 1.0.1 released

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-![](img/logo.png)
+![](img/logo_mini.png)
+
+A simple and transparent application to check exchange rates of currencies around the world. Just choose your base currency and convert its value to 30 most popular others.
+
+###powered by
+
+HTML, CSS, JS, jQ, JSON API (fixer.io)
+
--- js/main.js
+++ js/main.js
@@ -1,61 +1,107 @@
 $(function() {
-     //tiles
-     currencyData.forEach(function(tile) {
-        $('.currency-tiles').append(
-         '<div class="currency-tile mix" id="'+tile.id+'" data-order="'+tile.rating+'" data-name="'+tile.country+'">'+
-             '<div class="tile-header">'+
-                 '<div class="cur-name">'+tile.currencyName+' ('+tile.id+')</div>'+
-                 '<div class="cur-country">'+tile.country+'</div>'+
-             '</div>'+
-             '<div class="cur-exchange-value"></div>'+
-             '<div class="cur-course"><i class="fa fa-angle-up"'+'aria-hidden="true"></i></div>'+
-         '</div>'
-        )
- 
-        $('.currency-list > ol').append(
-         '<li class="currency-list-item"><div class="list-cur-id">'+tile.id+'</div><div class="list-cur-name">'+tile.currencyName+'</div></li>'
-        )
-     });
+    //js/currencies.js => currencyData || generating tiles to .currenncy-tiles
+    currencyData.forEach(function(tile) {
+        main.tileGenerator(tile)
+    });
+    //changing currency (.convert-form)
+    $('.currency-list-item').click(main.changeCurrency);
+    //currency list handling
+    $('.open-list-button, .overlay, .currency-list-item').click(main.listHandler);
+    //preview form convert handling
+    $('#convert-form, #preview-form').submit(function() {
+        main.convert($(this));
+        return false;
+    });
+    //if local storage doesn't exists - set default values (100 PLN)
+    main.setDefault();
+    // *** additionals ***
+        //open-close "about" modal
+    $('.open-about-modal').click(function() {
+        $('.about-modal').addClass('modal-opened');
+        $('.overlay-darken').addClass('overlay-visible');
+    })
+    $('.overlay-darken, .close-modal').click(function() {
+        $('.about-modal').removeClass('modal-opened');
+        $('.overlay-darken').removeClass('overlay-visible');
+    });
+        //mobile menu handling
+    $('.open-menu-button').click(function() {
+        $('.main-nav-mobile ol').fadeToggle(200);
+    });
+        //base currency amount input resizing
+    $('.base-currency-amount').keyup(main.resizeInput).each(main.resizeInput);
+        //overlays-height
+    $('.overlay, .overlay-darken').css('height',$(document).height()+'px');
+        //cookies info display
+    if (!localStorage.isCookiesAccept) $('.cookies-info').addClass('cookies-info-visible');
+    $('.close-cookies-info').click(function() {
+        localStorage.isCookiesAccept=true;
+        $('.cookies-info').removeClass('cookies-info-visible');
+    })
+        //scroll to top
+    $('.scroll-to-top').hide();
+    $(window).scroll(function() {
+        if($(window).scrollTop() < 200) $('.scroll-to-top').fadeOut();
+        else $('.scroll-to-top').fadeIn();
+    });
+    $('.scroll-to-top').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 600);
+    });
+        //mixitup init
+    var mixer = mixitup('.currency-tiles');
+});
 
-    //convert form currency changing
-
-    var changeCurrency = function() {
+var main = {
+    changeCurrency : function() {
         var curId = $(this).find('.list-cur-id');
         $('#currency-id').text(curId.text());
-    }
-
-    $('.currency-list-item').click(changeCurrency);
-
-    //preview display handling
-
-    var previewHandler = function(val,cur) {
+    },
+    previewHandler : function(val,cur) {
         $('.base-currency-id').text(cur);
-        var country;
 
+        var country;
         currencyData.forEach(function(curObj) {
             if (curObj.id == cur) country = curObj.country;
         })
        
         $('.base-currency-country').find('span').text(country);
-    }
+    },
+    listHandler : function() {
+        var button = $('.open-list-button');
 
-    //currency-list-handling
-
-    $('.open-list-button, .overlay, .currency-list-item').click(
-        function() {
-            var button = $('.open-list-button');
+        $('.currency-list, .overlay').fadeToggle(200);
+        if (button.hasClass('list-opened')) 
+            button.removeClass('list-opened')
+        else button.addClass('list-opened');
+    },
+    setDefault : function() {
+        if (!localStorage.value) localStorage.value = 100;
+        if (!localStorage.currency) localStorage.currency = 'PLN';
+        this.convert();
+    },
+    resizeInput : function() {
+        $(this).attr('size', $(this).val().length);
+        if (!$(this).val().length) $(this).attr('size', 1);
+    },
+    tileGenerator : function(tile) {
+        $('.currency-tiles').append(
+            '<div class="currency-tile mix" id="'+tile.id+'" data-order="'+tile.rating+'" data-name="'+tile.country+'">'+
+                '<div class="tile-header">'+
+                    '<div class="cur-name">'+tile.currencyName+' ('+tile.id+')</div>'+
+                    '<div class="cur-country">'+tile.country+'</div>'+
+                '</div>'+
+                '<div class="cur-exchange-value"></div>'+
+                '<div class="cur-course"><i class="fa fa-angle-up"'+'aria-hidden="true"></i></div>'+
+            '</div>'
+           )
     
-            $('.currency-list, .overlay').fadeToggle(200);
-            if (button.hasClass('list-opened')) 
-                button.removeClass('list-opened')
-            else button.addClass('list-opened');
-        }
-    );
-
-    //converting logic (getting data from form and json, then - converting)
-
-    var convert = function(element) {
-
+           $('.currency-list > ol').append(
+            '<li class="currency-list-item"><div class="list-cur-id">'+tile.id+'</div><div class="list-cur-name">'+tile.currencyName+'</div></li>'
+        )
+    },
+    convert : function(element) {
         if(element) {
             var value = element.find('input').val();
             var currency = $('#currency-id').html();
@@ -98,80 +144,7 @@ $(function() {
             })
         });
 
-        previewHandler(value,currency);
-
+        this.previewHandler(value,currency);
         $('input').val(value);
     }
-
-    // -- convert call - action --
-
-    $('#convert-form, #preview-form').submit(function() {
-        convert($(this));
-        return false;
-    });
-
-    // default convert values
-    
-    if (!localStorage.value) localStorage.value = 100;
-    if (!localStorage.currency) localStorage.currency = 'PLN';
-    convert();
-
-    //modal handling
-
-    $('.open-about-modal').click(function() {
-        $('.about-modal').addClass('modal-opened');
-        $('.overlay-darken').addClass('overlay-visible');
-    })
-
-    $('.overlay-darken, .close-modal').click(function() {
-        $('.about-modal').removeClass('modal-opened');
-        $('.overlay-darken').removeClass('overlay-visible');
-    });
-
-    //mobile menu handling
-
-    $('.open-menu-button').click(function() {
-        $('.main-nav-mobile ol').fadeToggle(200);
-    });
-
-    //base currency amount input resizing
-
-    function resizeInput() {
-        $(this).attr('size', $(this).val().length);
-        if (!$(this).val().length) $(this).attr('size', 1);
-    }
-
-    $('.base-currency-amount').keyup(resizeInput).each(resizeInput);
-
-    //overlays-height
-
-    $('.overlay, .overlay-darken').
-        css('height',$(document).height()+'px');
-    
-    //cookies info
-
-    if (!localStorage.isCookiesAccept) $('.cookies-info').addClass('cookies-info-visible');
-    
-    $('.close-cookies-info').click(function() {
-        localStorage.isCookiesAccept=true;
-        $('.cookies-info').removeClass('cookies-info-visible');
-    })
-
-    //scroll to top
-
-    $('.scroll-to-top').hide();
-    $(window).scroll(function() {
-        if($(window).scrollTop() < 200) $('.scroll-to-top').fadeOut();
-        else $('.scroll-to-top').fadeIn();
-    });
-    
-    $('.scroll-to-top').click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 600);
-    });
-    
-    //mixitup
-
-    var mixer = mixitup('.currency-tiles');
-})
+}
